cc9a34fa4d8339be794efc253d47ef5a1a81fc86
Trần Hồng Quân <thquan2k3@gmail.com>
2024-02-26 23:01:28 +0700

    add DOTween pro

---
+++ Assets/Third Party/StarterAssets/InputSystem/PlayerInputs.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+#if ENABLE_INPUT_SYSTEM
+using UnityEngine.InputSystem;
+#endif
+
+public class PlayerInputs : MonoBehaviour
+{
+    [Header("Character Input Values")]
+    public Vector2 move;
+    public Vector2 look;
+    public bool jump;
+    public bool sprint;
+    public bool changeView;
+    public bool isAim;
+    public bool isFire;
+
+    [Header("Movement Settings")]
+    public bool analogMovement;
+
+    [Header("Mouse Cursor Settings")]
+    public bool cursorLocked = true;
+    public bool cursorInputForLook = true;
+
+    [Header("Events")]
+    public UnityEvent onChangeView;
+    public UnityEvent onAim;
+    public UnityEvent onFire;
+
+#if ENABLE_INPUT_SYSTEM
+    public void OnMove(InputValue value)
+    {
+        MoveInput(value.Get<Vector2>());
+    }
+
+    public void OnLook(InputValue value)
+    {
+        if (cursorInputForLook)
+        {
+            LookInput(value.Get<Vector2>());
+        }
+    }
+
+    public void OnJump(InputValue value)
+    {
+        JumpInput(value.isPressed);
+    }
+
+    public void OnSprint(InputValue value)
+    {
+        SprintInput(value.isPressed);
+    }
+
+    public void OnChangeView(InputValue value)
+    {
+        ChangeView(value.isPressed);
+    }
+
+    public void OnFire(InputValue value)
+    {
+
+    }
+
+    public void OnAim(InputValue value)
+    {
+
+    }
+
+#endif
+
+
+    public void MoveInput(Vector2 newMoveDirection)
+    {
+        move = newMoveDirection;
+    }
+
+    public void LookInput(Vector2 newLookDirection)
+    {
+        look = newLookDirection;
+    }
+
+    public void JumpInput(bool newJumpState)
+    {
+        jump = newJumpState;
+    }
+
+    public void SprintInput(bool newSprintState)
+    {
+        sprint = newSprintState;
+    }
+
+    private void OnApplicationFocus(bool hasFocus)
+    {
+        SetCursorState(cursorLocked);
+    }
+
+    private void SetCursorState(bool newState)
+    {
+        Cursor.lockState = newState ? CursorLockMode.Locked : CursorLockMode.None;
+    }
+
+    private void ChangeView(bool newState)
+    {
+        changeView = newState;
+        onChangeView?.Invoke();
+    }
+
+    private void Aim(bool newState)
+    {
+        isAim = newState;
+        onAim?.Invoke();
+    }
+
+    private void Fire(bool newState)
+    {
+        isFire = newState;
+        onFire?.Invoke();
+    }
+}
